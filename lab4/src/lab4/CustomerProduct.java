eb67eda1387bef3536831bb506bc596c39904ee6
rodina <ra745126@gmail.com>
2025-10-18 23:49:48 +0300

    new Feature

--- lab4/src/lab4/CustomerProduct.java
+++ lab4/src/lab4/CustomerProduct.java
@@ -4,6 +4,46 @@
  */
 package lab4;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class CustomerProduct {
-    
+    private String  customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid=false;
+    }
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+    public String lineRepresentation(){
+         return customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid;
+    }
+    public boolean isPaid(){
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+    public String getSearchKey(){
+        return customerSSN + "," + productID + "," + purchaseDate.format(formatter);
+    }
+
 }
