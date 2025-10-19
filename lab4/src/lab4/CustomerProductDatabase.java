05f4c59961f95a1d32c738e63b38b315b07ba5ee
abdelrahman <abdoelsayedmo2005@gmail.com>
2025-10-19 20:36:47 +0300

    modifying  customer product database class

--- lab4/src/lab4/CustomerProductDatabase.java
+++ lab4/src/lab4/CustomerProductDatabase.java
@@ -4,7 +4,34 @@
  */
 package lab4;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class CustomerProductDatabase {
+
+
+public class CustomerProductDatabase extends Database {
+    
+    public CustomerProductDatabase (String filename){
+        super(filename);
+    }
+    
+    @Override
+    public Record createRecordFrom(String line) {
+       
+        
+       String[] parts = line.split(",");
+       
+        String customerSSN =  parts[0];
+        String productId = parts[1];
+        LocalDate purchaseDate = LocalDate.parse(parts[2], DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        boolean paid = Boolean.parseBoolean(parts[3]); //stil not sure
+        
+        CustomerProduct CP = new CustomerProduct(customerSSN,productId,purchaseDate);
+        
+        CP.setPaid(paid);
+      
+        return CP;
+    }
     
 }
+
