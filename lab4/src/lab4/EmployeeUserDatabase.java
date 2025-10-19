a46de42569a28eff5838241557191d4967876fcb
abdelrahman <abdoelsayedmo2005@gmail.com>
2025-10-19 20:37:47 +0300

    modifying  employee user database class

--- lab4/src/lab4/EmployeeUserDatabase.java
+++ lab4/src/lab4/EmployeeUserDatabase.java
@@ -5,6 +5,25 @@
 package lab4;
 
 
-public class EmployeeUserDatabase {
+public class EmployeeUserDatabase extends Database {
+    
+    public EmployeeUserDatabase(String filename) {
+        super(filename);
+    }
+    
+    @Override
+    public Record createRecordFrom(String line) {
+       
+        String[] parts = line.split(",");
+       
+        String employeeId =  parts[0];
+        String Name = parts[1];
+        String Email = parts[2];
+        String Address = parts[3];
+        String PhoneNumber = parts[4];
+        
+
+        return new EmployeeUser(employeeId,Name,Email,Address,PhoneNumber);
+    }
     
 }
